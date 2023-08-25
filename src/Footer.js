088489b7560f9b849529069c1b2a27d33b3ff389
Kushagra Mundra <kushsxtxn@gmail.com>
2023-08-26 04:21:10 +0530

    footer changes

--- src/Footer.js
+++ src/Footer.js
@@ -3,9 +3,14 @@ import React from 'react'
 const Footer = () => {
 
   let footerStyle = {
-    position: "relative",
-    top: "70vh",
-    width: "100%"
+    // position: "relative",
+    // top: "70vh",
+    // width: "100%"
+
+  position: 'fixed',
+  left: 0,
+  bottom: 0,
+  width: '100%',
   }
   return (
     <footer className='bg-dark text-light' style={footerStyle}>
